0de443460016d0a78c710444b58a2a7790f19e14
Xh Owais <87980770+XhOwais@users.noreply.github.com>
2023-08-04 19:04:15 +0500

    Vite Starter Template Upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+export default function App() {
+  return (
+    <>
+    <div className="main w-screen h-screen flex justify-center items-center">
+     <div className="Container w-8/12 h-2/4 rounded-xl flex justify-center items-center bg-gray-800 text-9xl font-bold text-white ">
+      <h1>Owais Shah</h1>
+     </div>
+     </div>
+    </>
+  )
+}
