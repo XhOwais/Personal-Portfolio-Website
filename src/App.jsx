9d6840fdc7f622a1f4837bd1b70351ff806e8976
Xh Owais <87980770+XhOwais@users.noreply.github.com>
2023-08-30 17:30:48 +0500

    Services section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Aboutme from "./Subcomponents/Aboutme";
 import Header from "./Subcomponents/Header";
 import Hero from "./Subcomponents/Hero";
+import Services from "./Subcomponents/Services";
 export default function App() {
   return (
     <>
       <Header/>
       <Hero/>
       <Aboutme/>
+      <Services/>
     </>
   )
 }
